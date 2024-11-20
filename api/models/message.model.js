8df39c953950d87a139756b08214e613b4fb1660
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-11-20 23:13:45 +0400

    7- Create Message Model

---
+++ api/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageShema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageShema);
+
+export default Message;
